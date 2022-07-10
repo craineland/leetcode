f5e14f88f31c56a6d0de39ca183f810d5f3484ae
Camden Landis <craine@vt.edu>
2022-07-10 11:16:38 -0400

    Added solution to challenge #13.

---
+++ roman_to_integer/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> numeralMap = new HashMap<Character, Integer>();
+        numeralMap.put('I', 1);
+        numeralMap.put('V', 5);
+        numeralMap.put('X', 10);
+        numeralMap.put('L', 50);
+        numeralMap.put('C', 100);
+        numeralMap.put('D', 500);
+        numeralMap.put('M', 1000);
+        
+        int total = 0;
+        
+        int i;
+        for (i = 0; i < s.length(); i++) {
+            // i>0 checks if index is past index 0
+            if (i+1 < s.length() && numeralMap.get(s.charAt(i)) < numeralMap.get(s.charAt(i+1))) {
+                total -= numeralMap.get(s.charAt(i));
+            }
+            else {
+                total += numeralMap.get(s.charAt(i));
+            }
+        }
+        return total;
+    }
+}
